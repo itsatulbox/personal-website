b9217a7998476647d7f6a9641e1eb0eee438990d
atulbox <kodlaatul@gmail.com>
2026-02-27 16:54:34 +1300

    feat: add scramble text animation component

---
+++ src/components/home/scrambletext.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const messages: string[] = [
+  "I'm a final year student at the University of Auckland",
+  "I'm an aspiring Software Engineer",
+  "I'm probably climbing right now",
+];
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+export default function ScrambleText() {
+  const [messageIndex, setMessageIndex] = useState(0);
+  const [message, setMessage] = useState(() =>
+    messages[0]
+      .split("")
+      .map((char) =>
+        char === " " ? " " : letters.charAt(Math.floor(Math.random() * letters.length))
+      )
+      .join("")
+  );
+
+  useEffect(() => {
+    let iterations = 0;
+    const target = messages[messageIndex];
+    const scrambleInterval = setInterval(() => {
+      const newMessage = target
+        .split("")
+        .map((char, index) => {
+          if (char === " " || index < iterations) return char;
+          return letters.charAt(Math.floor(Math.random() * letters.length));
+        })
+        .join("");
+
+      setMessage(newMessage);
+
+      iterations += 1 / 3;
+
+      if (iterations >= target.length) {
+        clearInterval(scrambleInterval);
+        setTimeout(() => {
+          setMessageIndex((prev) => (prev + 1) % messages.length);
+        }, 7500);
+      }
+    }, 15);
+
+    return () => clearInterval(scrambleInterval);
+  }, [messageIndex]);
+
+  return (
+    <p className="text-xl h-[5.5rem]" suppressHydrationWarning>
+      Hello!
+      <br />
+      I&apos;m Atul and {message}
+    </p>
+  );
+}
